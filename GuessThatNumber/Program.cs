435b80f694a26e2ec4040c4440cad94085a879ae
SeedPaths-User <lessdismissive@email.com>
2014-04-17 16:50:05 -0600

    Update for EXTRA CREDIT
    Input a Warmer/Colder system, as well as offered a choice to play again.

--- GuessThatNumber/Program.cs
+++ GuessThatNumber/Program.cs
@@ -22,31 +22,61 @@ namespace GuessThatNumber
             var cpuNum = random.Next(0,100); //Var = cpu selection
             var playerNum = 0; //var = player attempts
             Console.WriteLine("Ready to play GUESS THAT NUMBER?!?! Choose your 1st number (1-100)!");
-             
             var input = Console.ReadLine();
             var isNum = Convert.ToInt32(input); //Converts player string to int.
-            while(isNum != cpuNum) //While player choice doesnt = cpu choice, loop will keep running
+            while (isNum != cpuNum) //While player choice doesnt = cpu choice, loop will keep running
             {
                 if (isNum < cpuNum) //if player choice is less, this will run
                 {
                     Console.WriteLine("Not quite... Go higher!");
-                    input = Console.ReadLine();
-                    isNum = Convert.ToInt32(input);
-                    playerNum +=1;
+                    //EXTRA CREDIT
+                    //When plaayer is way off or near, will display Warmer/Colder
+                    if (cpuNum - isNum < 20)
+                    {
+                        Console.WriteLine("Getting warm!");
+                    }
+                    if (cpuNum - isNum > 60)
+                    {
+                        Console.WriteLine("Icey cold! >,<");
+                    }
+                    input = Console.ReadLine();// Player choice is updated
+                    isNum = Convert.ToInt32(input);//Then converted into Int again   
+                    playerNum += 1; //Attempt counter +1  
+                    
                 }
-                if (isNum > cpuNum)
+                else if (isNum > cpuNum)// If player choice is more than Cpu choice
                 {
-                    Console.WriteLine("Ouch, too high! Try again.");
+                    Console.WriteLine("Ouch, too high! Try again.");//repeat above
+                    //EXTRA CREDIT
+                    if (isNum - cpuNum < 20)
+                    {
+                        Console.WriteLine("Getting warm!");
+                    }
+                    if (isNum - cpuNum > 60)
+                    {
+                        Console.WriteLine("Icey cold! >,<");
+                    }
+
                     input = Console.ReadLine();
                     isNum = Convert.ToInt32(input);
-                    playerNum +=1;
+                    playerNum += 1;
                 }
-                
             }
-                    Console.WriteLine("CONGRATULATIONS!! The number WAS: " + cpuNum);
-                    Console.WriteLine("Attempts: " + playerNum);
-                
-            
+            Console.WriteLine("CONGRATULATIONS!! The number WAS: " + cpuNum); //Player won!! 
+            Console.WriteLine("Attempts: " + playerNum); 
+            Console.WriteLine(" ");
+            //EXTRA CREDIT
+            //asks if player would like to play again, then starts function again from beginning
+            Console.WriteLine("Would you like to play GUESS THAT NUMBER!! Again?");
+            var choice = Console.ReadLine();
+            if (choice != "no")
+            {
+                GuessNumber();
+            }
+            else
+            {
+                Console.WriteLine("Thanks for playing! Please get away from me now!");
+            }
         }
     }
 }
